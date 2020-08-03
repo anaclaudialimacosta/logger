ed591ce9fb7061c744f8b5735935f54a3ded3445
anaclaudia <almeidannynha@gmail.com>
2020-08-03 19:12:18 -0300

    Adding Twitter Integration

--- src/LoggerHandlers/LoggerFileHandler.php
+++ src/LoggerHandlers/LoggerFileHandler.php
@@ -4,18 +4,26 @@ namespace MyLogger\LoggerHandlers;
 
 use MyLogger\Interfaces\MyLoggerInterface;
 use MyLogger\Formatter\LogInFileFormatter;
+use PHPUnit\Util\Exception;
 
 class LoggerFileHandler implements MyLoggerInterface
 {
+    private $logInFileFormatter;
+
+    public function __construct(LogInFileFormatter $logInFileFormatter)
+    {
+        $this->logInFileFormatter = $logInFileFormatter;
+    }
+
     public function log(string $log, array $logger)
     {
         if(empty($logger['file']))
         {
-           throw new \RuntimeException("File's Path Not Defined");
+           throw new Exception("File's Path Not Defined");
         }
         $loggerPath = $logger['file'];
         $file = fopen($loggerPath, 'w');
-        fwrite($file, formatLog($log));
+        fwrite($file, $this->logInFileFormatter->formatLog($log));
         fclose($file);
     }
 }
--- src/LoggerHandlers/LoggerTweetHandler.php
+++ src/LoggerHandlers/LoggerTweetHandler.php
@@ -2,12 +2,20 @@
 
 namespace MyLogger\LoggerHandlers;
 
+use http\Exception\RuntimeException;
 use Loggyfier\Interfaces\MyLoggerInterface;
+use PHPUnit\Util\Exception;
+
+require_once ('../../config.php');
 
 class LoggerTweetHandler implements  MyLoggerInterface
 {
     public function log(string $log, array $logger)
     {
+        if(empty($logger['tweet']))
+        {
+            throw new Exception('Twitter Was Not Requested') ;
+        }
 
     }
 }
